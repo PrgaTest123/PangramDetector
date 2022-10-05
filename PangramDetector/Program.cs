144db87302f504f3fe6172b85644f1645d557778
PrgaTest123 <marintest0@gmail.com>
2022-10-05 22:06:33 +0200

    Add project files.

---
+++ PangramDetector/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace PangramDetector
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter words: ");
+            string str = Console.ReadLine();
+            Console.WriteLine(IsPangram(str));
+        }
+
+        public static bool IsPangram(string str)
+        {
+            //You can use Distinct() in exchange for GroupBy()
+            return str.ToLower().Where(ch=> Char.IsLetter(ch)).GroupBy(ch=> ch).Count()==26;
+        }
+    }
+}
